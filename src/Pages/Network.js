61cb9f8bd9a80a8486962ac878af41c4bb0f3511
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-06-07 17:26:49 +0530

    table

--- src/Pages/Network.js
+++ src/Pages/Network.js
@@ -4,96 +4,97 @@ import React from "react";
 import Header from "../Header/Header";
 import { DataGrid } from "@mui/x-data-grid";
 import { IconButton } from "@mui/material";
+import "./Network.css";
 
 export const Network = () => {
-  const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "email", headerName: "Email", width: 250 },
+  // const columns = [
+  //   { field: "id", headerName: "ID", width: 70 },
+  //   { field: "email", headerName: "Email", width: 250 },
 
-    // { field: "firstName", headerName: "First name", width: 130 },
-    // { field: "lastName", headerName: "Last name", width: 130 },
-    {
-      field: "fullName",
-      headerName: "Full Name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-    },
-    { field: "mobileNumber", headerName: "Mobile Number", width: 150 },
-    { field: "mobileVerified", headerName: "Mobile Verified", width: 150 },
-    { field: "emailVerifiied", headerName: "Email Verified", width: 120 },
-    { field: "remove", headerName: "Remove", width: 150 },
-  ];
-  const rows = [
-    {
-      id: 1,
-      email: "elitekanishkraj@gmail.com",
-      lastName: "Snow",
-      firstName: "Jon",
-      mobileNumber: "9876543210",
-      mobileVerified: "Yes",
-      emailVerified: "Yes",
-      remove: `${(<Close />)}`,
-    },
-    {
-      id: 2,
-      email: "testerqjhwghbiekjafh9wekjfhn @gmail.com",
-      lastName: "Lannister",
-      firstName: "Cersei",
-      mobileNumber: "9876543210",
-    },
-    {
-      id: 3,
-      email: "testing@gmail.com",
-      lastName: "Lannister",
-      firstName: "Jaime",
-    },
-    {
-      id: 4,
-      email: "starkarya@gmail.com",
-      lastName: "Stark",
-      firstName: "Arya",
-    },
-    {
-      id: 5,
-      email: "Lord@gmail.com",
-      lastName: "Targaryen",
-      firstName: "Daenerys",
-    },
-    {
-      id: 6,
-      email: "Rahil@gmail.com",
-      lastName: "Melisandre",
-      firstName: "Rahil",
-    },
-    {
-      id: 7,
-      email: "ferrara@gmail.com",
-      lastName: "Clifford",
-      firstName: "Ferrara",
-    },
-    {
-      id: 8,
-      email: "Rossini@gmail.com",
-      lastName: "Frances",
-      firstName: "Rossini",
-    },
-    {
-      id: 9,
-      email: "Harvey@gmail.com",
-      lastName: "Roxie",
-      firstName: "Harvey",
-    },
-  ];
+  //   { field: "firstName", headerName: "First name", width: 130 },
+  //   { field: "lastName", headerName: "Last name", width: 130 },
+  //   {
+  //     field: "fullName",
+  //     headerName: "Full Name",
+  //     description: "This column has a value getter and is not sortable.",
+  //     sortable: false,
+  //     width: 160,
+  //     valueGetter: (params) =>
+  //       `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  //   },
+  //   { field: "mobileNumber", headerName: "Mobile Number", width: 150 },
+  //   { field: "mobileVerified", headerName: "Mobile Verified", width: 150 },
+  //   { field: "emailVerifiied", headerName: "Email Verified", width: 120 },
+  //   { field: "remove", headerName: "Remove", width: 150 },
+  // ];
+  // const rows = [
+  //   {
+  //     id: 1,
+  //     email: "elitekanishkraj@gmail.com",
+  //     lastName: "Snow",
+  //     firstName: "Jon",
+  //     mobileNumber: "9876543210",
+  //     mobileVerified: "Yes",
+  //     emailVerified: "Yes",
+  //     remove: `${(<Close />)}`,
+  //   },
+  //   {
+  //     id: 2,
+  //     email: "testerqjhwghbiekjafh9wekjfhn @gmail.com",
+  //     lastName: "Lannister",
+  //     firstName: "Cersei",
+  //     mobileNumber: "9876543210",
+  //   },
+  //   {
+  //     id: 3,
+  //     email: "testing@gmail.com",
+  //     lastName: "Lannister",
+  //     firstName: "Jaime",
+  //   },
+  //   {
+  //     id: 4,
+  //     email: "starkarya@gmail.com",
+  //     lastName: "Stark",
+  //     firstName: "Arya",
+  //   },
+  //   {
+  //     id: 5,
+  //     email: "Lord@gmail.com",
+  //     lastName: "Targaryen",
+  //     firstName: "Daenerys",
+  //   },
+  //   {
+  //     id: 6,
+  //     email: "Rahil@gmail.com",
+  //     lastName: "Melisandre",
+  //     firstName: "Rahil",
+  //   },
+  //   {
+  //     id: 7,
+  //     email: "ferrara@gmail.com",
+  //     lastName: "Clifford",
+  //     firstName: "Ferrara",
+  //   },
+  //   {
+  //     id: 8,
+  //     email: "Rossini@gmail.com",
+  //     lastName: "Frances",
+  //     firstName: "Rossini",
+  //   },
+  //   {
+  //     id: 9,
+  //     email: "Harvey@gmail.com",
+  //     lastName: "Roxie",
+  //     firstName: "Harvey",
+  //   },
+  // ];
   return (
     <>
       <Header />
       <div className="main">
         <div className="container">
           <div className="row">
-            <Typography variant="h6" sx={{ mt: 3 }}>
+            {/* <Typography variant="h6" sx={{ mt: 3 }}>
               Network <ChevronRight />
             </Typography>
             <div className="col-sm-12">
@@ -105,6 +106,59 @@ export const Network = () => {
                   rowsPerPageOptions={[5]}
                 />
               </div>
+            </div> */}
+
+            <div style={{ overflow: "auto" }}>
+              <table>
+                <tr style={{ backgroundColor: "#5322E5", color: "white" }}>
+                  <th className="px-2">ID</th>
+                  <th className="px-2">Email</th>
+                  <th className="px-2">Full Name</th>
+                  <th className="px-2">Mobile Number</th>
+                  <th className="px-2">Email Verified</th>
+                  <th className="px-2">Remove</th>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Kanishk</td>
+                  <td>kanish</td>
+                  <td>89875d</td>
+                  <td>hfhsadgg987</td>
+                  <td>yes</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Kanishk</td>
+                  <td>kanish</td>
+                  <td>89875d</td>
+                  <td>hfhsadgg987</td>
+                  <td>yes</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Kanishk</td>
+                  <td>kanish</td>
+                  <td>89875d</td>
+                  <td>hfhsadgg987</td>
+                  <td>yes</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Kanishk</td>
+                  <td>kanish</td>
+                  <td>89875d</td>
+                  <td>hfhsadgg987</td>
+                  <td>yes</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Kanishk</td>
+                  <td>kanish</td>
+                  <td>89875d</td>
+                  <td>hfhsadgg987</td>
+                  <td>yes</td>
+                </tr>
+              </table>
             </div>
           </div>
         </div>
