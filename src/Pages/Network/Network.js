bd496bdf648f385da083fb6d2d0756b7359bf3e5
@kanishk900 <walkerrahil@gmail.com>
2022-06-10 13:28:50 +0530

    a

--- src/Pages/Network/Network.js
+++ src/Pages/Network/Network.js
@@ -1,71 +1,14 @@
 import { Button, Typography } from "@material-ui/core";
 import { FormControl, IconButton, InputLabel, MenuItem } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Header from "../../Header/Header";
 import "./Network.css";
 import Select from "@mui/material/Select";
 import { Delete } from "@mui/icons-material";
+import GlobalContext from "../../Context/GlobalContext";
 
-export const Network = () => {
-  const [select] = useState();
-  const [approvedData, setApproved] = useState([
-    {
-      id: 1,
-      isApproved: "approved",
-    },
-    {
-      id: 2,
-      isApproved: "approved",
-    },
-    {
-      id: 3,
-      isApproved: "reject",
-    },
-    {
-      id: 4,
-      isApproved: "approved",
-    },
-    {
-      id: 5,
-      isApproved: "reject",
-    },
-    {
-      id: 6,
-      isApproved: "approved",
-    },
-  ]);
-  const [dataCopy, setDataCopy] = useState([...approvedData]);
-
-  const deleteRow = (id) => {
-    const newData = [...approvedData];
-    const deletedData = newData.filter((item, index) => {
-      return index !== id;
-    });
-    setApproved(deletedData);
-  };
-  // const filterdata = (value) => {
-  //   console.log("va", value);
-  //   const newArr = [...filterData];
-  //   const fitlerd = newArr.filter((item) => {
-  //     return item.isApproved === value;
-  //   });
-  //   console.log(fitlerd);
-  //   setApproved(fitlerd);
-  // };
-  // useEffect(() => {
-  //   setFilterData(approvedData);
-  // }, []);
-  const filterdata = (value) => {
-    console.log("va", value);
-    // const newArr = [...approvedData];
-    // console.log("newArr", newArr);
-    const fitlerd = dataCopy.filter((item) => {
-      return item.isApproved === value;
-    });
-    console.log("filtereddata", fitlerd);
-    setApproved(fitlerd);
-  };
-
+export const Network = (props) => {
+  const { getAllNetworkData, network } = useContext(GlobalContext);
   return (
     <>
       <Header />
@@ -85,9 +28,7 @@ export const Network = () => {
                     <Select
                       labelId="demo-simple-select-label"
                       id="demo-simple-select"
-                      value={select}
                       label="Select"
-                      onChange={(e) => filterdata(e.target.value)}
                     >
                       <MenuItem value="approved">Approved</MenuItem>
                       <MenuItem value="reject">Reject</MenuItem>
@@ -102,26 +43,27 @@ export const Network = () => {
                   <tbody className="text-center">
                     <tr className="tablebody">
                       <th>Profile</th>
-                      <th>Email</th>
-                      <th>Founder Name</th>
+                      <th>Newtorks</th>
+                      <th>​ compliance_officer</th>
                       <th>Actions</th>
+                      <th>View</th>
                       <th>Remove</th>
                     </tr>
-                    {approvedData.map((item, index) => (
-                      <tr index>
+                    {network.map((item) => (
+                      <tr>
                         <td>
                           <img
                             className="rounded-circle"
                             style={{ width: "40px" }}
-                            src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg"
+                            src=""
                             alt="user"
                           />
                         </td>
                         <td>
-                          <h6 className="mt-2">elite@gmail.com</h6>
+                          <h6 className="mt-2">{item.name} </h6>
                         </td>
                         <td>
-                          <h6 className="mt-2 ">Anurag</h6>
+                          <h6 className="mt-2 ">{item.compliance_officer} </h6>
                         </td>
                         <td>
                           <div className="mt-2">
@@ -129,23 +71,24 @@ export const Network = () => {
                               variant="outlined"
                               size="small"
                               className="mx-2"
-                              color="default"
+                              color="secondary"
                               style={{ width: "94px" }}
                             >
-                              {item.isApproved}
+                              {item.is_approved ? "Approved" : "Reject"}
                             </Button>
-                            {/* 
-                            <Button
-                              variant="outlined"
-                              size="small"
-                              color="primary"
-                            >
-                              Approve
-                            </Button> */}
                           </div>
                         </td>
                         <td>
-                          <IconButton onClick={() => deleteRow(index)}>
+                          <Button
+                            variant="outlined"
+                            size="small"
+                            color="default"
+                          >
+                            View{" "}
+                          </Button>
+                        </td>
+                        <td>
+                          <IconButton>
                             <Delete color="secondary" />
                           </IconButton>
                         </td>
