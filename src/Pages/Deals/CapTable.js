2c74ad68a041b017b7dcf52db83b49b5e1a63da5
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-06-21 19:14:54 +0530

    a

--- src/Pages/Deals/CapTable.js
+++ src/Pages/Deals/CapTable.js
@@ -8,13 +8,31 @@ import Header from "../../Header/Header";
 export default function CapTable() {
   const current = window.location.pathname;
   const [capTable, setCapTable] = useState([]);
+  const [totalShares, setTotalShares] = useState();
 
   const getCapTable = async () => {
-    var res = await getRequest("/dashboard/startup/captable", true);
+    var res = await getRequest("/dashboard/captable", true);
     console.log("res", res);
     var responseData = await res.json();
     console.log("responseData", responseData);
     setCapTable(responseData);
+    getSharesSum(responseData);
+  };
+  const getSharesSum = (row) => {
+    try {
+      if (!row) return;
+      // console.log('row', row)
+      const sum = row.reduce(
+        (total, currentValue) => (total = total + currentValue.no_of_shares),
+        0
+      );
+      // console.log("sumShares", sum);
+      // const percentage = (12 / sum) * 100;
+      // console.log("percent", percentage);
+      setTotalShares(sum);
+    } catch (err) {
+      console.log("err", err);
+    }
   };
   useEffect(() => {
     getCapTable();
@@ -62,7 +80,7 @@ export default function CapTable() {
             <div className="col-sm-12 mt-sm-3">
               <Typography>Cap Table</Typography>
             </div>
-            <div className="px-sm-0 px-md-2 px-lg-5 pt-4">
+            <div className="px-sm-0 px-md-2 px-lg-1 pt-4">
               <div className="px-sm-0 px-md-2 px-lg-5 table-responsive">
                 <table className="table border">
                   <thead className="text-center">
@@ -70,6 +88,11 @@ export default function CapTable() {
                       <th>
                         <Typography variant="h6">
                           Network <br /> Name
+                        </Typography>
+                      </th>
+                      <th>
+                        <Typography variant="h6">
+                          Startup <br /> Name
                         </Typography>
                       </th>
                       <th className="pb-4">
@@ -99,29 +122,44 @@ export default function CapTable() {
                     </tr>
                   </thead>
                   <tbody className="text-center">
-                    <tr className="tableHover">
-                      <td>
-                        <h6 className="mt-2">angel network</h6>
-                      </td>
-                      <td>
-                        <h6 className="mt-2">Bootstrap</h6>
-                      </td>
-                      <td>
-                        <h6 className="mt-2">Shekhar</h6>
-                      </td>
-                      <td>
-                        <h6 className="mt-2">29 june 2004</h6>
-                      </td>
-                      <td>
-                        <h6 className="mt-2">23</h6>
-                      </td>
-                      <td>
-                        <h6 className="mt-2">29%</h6>
-                      </td>
-                      <td>
-                        <h6 className="mt-2">10000000000</h6>
-                      </td>
-                    </tr>
+                    {capTable.map((item, id) => (
+                      <tr key={id} className="tableHover">
+                        <td>
+                          <h6 className="mt-2">
+                            {item.network_name ? item.network_name : "No Data"}
+                          </h6>
+                        </td>
+                        <td>
+                          <h6 className="mt-2">{item.startup_name}</h6>
+                        </td>
+                        <td>
+                          <h6 className="mt-2">{item.stage}</h6>
+                        </td>
+                        <td>
+                          <h6 className="mt-2">{item.name}</h6>
+                        </td>
+                        <td>
+                          <h6 className="mt-2">{item.date_of_allotment}</h6>
+                        </td>
+                        <td>
+                          <h6 className="mt-2">{item.no_of_shares}</h6>
+                        </td>
+                        <td>
+                          <h6 className="mt-2">
+                            {parseFloat(
+                              (item.no_of_shares / totalShares) * 100
+                            ).toFixed(2)}
+                            %
+                          </h6>
+                        </td>
+                        <td>
+                          <h6 className="mt-2">
+                            {parseFloat(item.price_per_instruement) *
+                              item.no_of_shares}
+                          </h6>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
