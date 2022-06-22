a80feab316ae8e25d90fb2cafbacbaa82c7903a1
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-06-22 16:50:29 +0530

    Aa

--- src/Pages/Startup/StartUpTeam.js
+++ src/Pages/Startup/StartUpTeam.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRequest } from "../../Constant/apiCall";
 import Header from "../../Header/Header";
+import NetworkEmpty from "../Empty/NetworkEmpty";
 import StartUpTopBar from "./StartUpTopBar";
 
 export default function StartUpTeam() {
@@ -28,54 +29,64 @@ export default function StartUpTeam() {
             <div className="col-sm-12 mt-sm-3">
               <Typography variant="h6">Teams</Typography>
             </div>
-            <div className="px-md-2 px-lg-4 pt-4">
-              <div className="px-md-2 px-lg-4 table-responsive">
-                <table className="table border">
-                  <thead className="text-center">
-                    <tr className="tablebody headHover">
-                      <th>
-                        <Typography variant="h6">Name</Typography>
-                      </th>
-                      <th>
-                        <Typography variant="h6">Designation</Typography>
-                      </th>
-                      <th>
-                        <Typography variant="h6">Designation 2</Typography>
-                      </th>
-                      <th>
-                        <Typography variant="h6">Joining Date</Typography>
-                      </th>
-                      <th>
-                        <Typography variant="h6">
-                          Years of Experience
-                        </Typography>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="text-center">
-                    {team.map((item, id) => (
-                      <tr key={id} className="tableHover">
-                        <td>
-                          <h6 className="mt-2">{item.name}</h6>
-                        </td>
-                        <td>
-                          <h6 className="mt-2">{item.category1}</h6>
-                        </td>
-                        <td>
-                          <h6 className="mt-2">{item.category2}</h6>
-                        </td>
-                        <td>
-                          <h6 className="mt-2">{item.joining_date}</h6>
-                        </td>
-                        <td>
-                          <h6 className="mt-2">{item.year_of_experience}</h6>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
+            {team.length > 0 ? (
+              <>
+                <div className="px-md-2 px-lg-4 pt-4">
+                  <div className="px-md-2 px-lg-4 table-responsive">
+                    <table className="table border">
+                      <thead className="text-center">
+                        <tr className="tablebody headHover">
+                          <th>
+                            <Typography variant="h6">Name</Typography>
+                          </th>
+                          <th>
+                            <Typography variant="h6">Designation</Typography>
+                          </th>
+                          <th>
+                            <Typography variant="h6">Designation 2</Typography>
+                          </th>
+                          <th>
+                            <Typography variant="h6">Joining Date</Typography>
+                          </th>
+                          <th>
+                            <Typography variant="h6">
+                              Years of Experience
+                            </Typography>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="text-center">
+                        {team.map((item, id) => (
+                          <tr key={id} className="tableHover">
+                            <td>
+                              <h6 className="mt-2">{item.name}</h6>
+                            </td>
+                            <td>
+                              <h6 className="mt-2">{item.category1}</h6>
+                            </td>
+                            <td>
+                              <h6 className="mt-2">{item.category2}</h6>
+                            </td>
+                            <td>
+                              <h6 className="mt-2">{item.joining_date}</h6>
+                            </td>
+                            <td>
+                              <h6 className="mt-2">
+                                {item.year_of_experience}
+                              </h6>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <NetworkEmpty />
+              </>
+            )}
           </div>
         </div>
       </div>
