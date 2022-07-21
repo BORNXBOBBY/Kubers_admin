079aac9f27cca8e589f64e55ca8c671a356d716b
Aman Sahani <sahaniaman867@gmail.com>
2022-07-21 04:31:38 -0400

    a

--- src/Pages/Startup/Documents.js
+++ src/Pages/Startup/Documents.js
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 import { getRequest } from "../../Constant/apiCall";
 import { addSubStr } from "../../Constant/Substring";
 import Header from "../../Header/Header";
+import { Network } from "../Network/Network";
+import NetworksSkeleton from "../Skeleton/NetworksSkeleton";
 
 export default function Documents() {
   const [startupDoc, setStartupDoc] = useState([]);
+  const [docSkeleton, setDocSkeleton] = useState(true);
 
   const getDocument = async () => {
     try {
@@ -16,6 +19,7 @@ export default function Documents() {
       var responseData = await res.json();
       console.log("responseData", responseData);
       setStartupDoc(responseData);
+      setDocSkeleton(false);
     } catch (e) {
       console.log(e);
     }
@@ -26,16 +30,15 @@ export default function Documents() {
   }, []);
   const current = window.location.pathname;
 
-
   return (
     <>
       <Header />
       <div className="main">
-      <div className="container">
-      <div className="d-flex border-bottom">
+        <div className="container">
+          <div className="d-flex border-bottom">
             <span className="">
               {" "}
-              <Link 
+              <Link
                 className={`${
                   current === "/startup" ? "topLink-active" : "topLink"
                 }`}
@@ -47,7 +50,9 @@ export default function Documents() {
             <span className="">
               <Link
                 className={`${
-                  current === "/startup/documents" ? "topLink-active" : "topLink"
+                  current === "/startup/documents"
+                    ? "topLink-active"
+                    : "topLink"
                 }`}
                 to="/startup/documents"
               >
@@ -55,49 +60,63 @@ export default function Documents() {
               </Link>
             </span>
           </div>
-        <div className="px-sm-5 px-lg-5 px-md-0">
-          <div class="table-responsive">
-            <table class="table">
-              <thead class="thead-dark">
-                <tr align="center">
-                  <th scope="col">Document Type</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Date</th>
-                  <th scope="col">Documents</th>
-                </tr>
-              </thead>
-              {startupDoc.map((item, id) => (
-                <tbody>
-                  <tr align="center">
-                    <td> {item.document_type === 'pitch_desk' ? 'Pitch Desk': item.document_type === 'startupdocuments' ? 'Startup Documents': item.document_type}</td>
-                    <td>{addSubStr(item.description, 30)}</td>
-                    <td>
-                      {new Date(item.uploaded_date_time).toLocaleDateString()}
-                    </td>
-                    <td>
-                      <>
-                        <Button
-                          className="link"
-                          target="_blank"
-                          href={item.documents}
-                          rel="noreferrer"
-                          variant="outlined"
-                          size="small"
-                          hre
-                        >
-                          View
-                        </Button>
-                      </>
-                    </td>
-                  </tr>
-                </tbody>
-              ))}
-            </table>
-          </div>
+          {docSkeleton ? (
+            <NetworksSkeleton />
+          ) : (
+            <>
+              <div className="px-sm-5 px-lg-5 px-md-0">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class="thead-dark">
+                      <tr align="center">
+                        <th scope="col">Document Type</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Documents</th>
+                      </tr>
+                    </thead>
+                    {startupDoc.map((item, id) => (
+                      <tbody>
+                        <tr align="center">
+                          <td>
+                            {" "}
+                            {item.document_type === "pitch_desk"
+                              ? "Pitch Desk"
+                              : item.document_type === "startupdocuments"
+                              ? "Startup Documents"
+                              : item.document_type}
+                          </td>
+                          <td>{addSubStr(item.description, 30)}</td>
+                          <td>
+                            {new Date(
+                              item.uploaded_date_time
+                            ).toLocaleDateString()}
+                          </td>
+                          <td>
+                            <>
+                              <Button
+                                className="link"
+                                target="_blank"
+                                href={item.documents}
+                                rel="noreferrer"
+                                variant="outlined"
+                                size="small"
+                                hre
+                              >
+                                View
+                              </Button>
+                            </>
+                          </td>
+                        </tr>
+                      </tbody>
+                    ))}
+                  </table>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
-      </div>
-      
     </>
   );
 }
